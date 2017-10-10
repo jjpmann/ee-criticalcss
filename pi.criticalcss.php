ce8b9801510231cd93825a86122e347eb9bf6970
Jerry Price <jerryprice@lmo.com>
2017-10-09 23:20:34 -0400

    ready

--- pi.criticalcss.php
+++ pi.criticalcss.php
@@ -15,29 +15,26 @@ require_once('config.php');
 class criticalcss {
 
     protected $bail;
+    protected $base;
 
     public function __construct()
     {
         $this->bail = ee()->config->item('critical_css') === 'off';
+        $this->base = ee()->config->item('critical_css_base') ?: '/assets/css/critical/';
     }
 
     public function run()
     {
 
-        echo "<pre>".__FILE__.'<br>'.__METHOD__.' : '.__LINE__."<br><br>"; var_dump( ee()->config->item('critical_css') ); exit;
-        
-
         if ($this->bail) {
             return;
         }
-
-        $base = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/critical/';
         
         $name = ee()->TMPL->fetch_param('name');
 
-        $file = $base . $name .'.css';
+        $file = $this->base . $name .'.css';
         if (ENV !== 'local') {
-            $file = $base . $name .'.min.css';
+            $file = $this->base . $name .'.min.css';
         }
 
         if (file_exists($file)) {
@@ -45,6 +42,9 @@ class criticalcss {
             return "<!-- {$name} --><style media='screen'>{$css}</style>";
         }
 
+        if (DEBUG) {
+            return "<!-- criticalcss: {$file} -->";
+        }
         return;
     }
 
