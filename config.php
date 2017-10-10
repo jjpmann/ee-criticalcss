abe1b27c2eeb3a65d75f23fea32970cc98543204
Jerry Price <jerryprice@lmo.com>
2017-10-09 23:21:15 -0400

    no message

--- config.php
+++ config.php
@@ -13,6 +13,6 @@ if ( !defined('CRITICALCSS_NAME') )
     // Basic Add-on settings
     define('CRITICALCSS_NAME', 'CriticalCSS');
     define('CRITICALCSS_VERSION', '0.0.1');
-    define('CRITICALCSS_DESCRIPTION', 'CriticalCSS helpers');
+    define('CRITICALCSS_DESCRIPTION', 'CriticalCSS for ExpressionEngine');
     define('CRITICALCSS_MOD_NAME', 'CriticalCSS');
 }
