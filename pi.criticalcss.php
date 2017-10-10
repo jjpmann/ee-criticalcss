a836c9527fdcfdf671ee64f21df3b9e69845db9c
Jerry Price <jerryprice@lmo.com>
2017-10-09 22:57:33 -0400

    update

--- pi.criticalcss.php
+++ pi.criticalcss.php
@@ -14,9 +14,11 @@ require_once('config.php');
 
 class criticalcss {
 
+    protected $bail;
+
     public function __construct()
     {
-
+        $this->bail = ee()->config->item('critical_css') === 'off';
     }
 
     public function run()
@@ -25,12 +27,14 @@ class criticalcss {
         echo "<pre>".__FILE__.'<br>'.__METHOD__.' : '.__LINE__."<br><br>"; var_dump( ee()->config->item('critical_css') ); exit;
         
 
-        if (ee()->config->item('critical_css') === 'off') {
+        if ($this->bail) {
             return;
         }
 
         $base = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/critical/';
+        
         $name = ee()->TMPL->fetch_param('name');
+
         $file = $base . $name .'.css';
         if (ENV !== 'local') {
             $file = $base . $name .'.min.css';
@@ -38,7 +42,7 @@ class criticalcss {
 
         if (file_exists($file)) {
             $css = file_get_contents($file);
-            return "<!-- {$name} --><style media='screen'>.forPrint{display:none;}{$css}</style>";
+            return "<!-- {$name} --><style media='screen'>{$css}</style>";
         }
 
         return;
